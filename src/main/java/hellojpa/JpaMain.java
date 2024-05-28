ebc8cb9a1264d1ad34f7de6f4d4e8edcda184c35
idohyeon <ehgus5026@naver.com>
2024-05-28 19:04:31 +0900

    -Jpa Setting

--- src/main/java/hellojpa/JpaMain.java
+++ src/main/java/hellojpa/JpaMain.java
@@ -8,9 +8,9 @@ public class JpaMain {
 
         // persistence.xml의 unit name을 넘겨주기
         EntityManagerFactory emf = Persistence.createEntityManagerFactory("hello");
-        EntityManager em = emf.createEntityManager();
-        //code
+        EntityManager em = emf.createEntityManager(); // 엔티티 매니저 꺼내기
 
+        //code(crud와 같은 기능 돌아간 후 엔티티 매니저와 엔티티 매니저 팩토리 모두 꺼야함.)
         em.close();
         emf.close();
     }
