162cb59dda33d3696d0c2c6ed041e91dcb57ec20
idohyeon <ehgus5026@naver.com>
2024-06-01 01:35:44 +0900

    -@OneToOne

---
+++ src/main/java/hellojpa/Locker.java
@@ -0,0 +1,19 @@
+package hellojpa;
+
+import jakarta.persistence.*;
+
+@Entity
+public class Locker {
+
+    @Id @GeneratedValue
+    @Column(name = "LOCKER_ID")
+    private long id;
+
+    private String name;
+
+    // 일대일 양방향
+    @OneToOne(mappedBy = "locker")
+    private Member member;
+
+
+}
