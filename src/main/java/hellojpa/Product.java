bfbe0511364421f4a7b9428cecfd3812f3f2142e
idohyeon <ehgus5026@naver.com>
2024-06-01 23:37:36 +0900

    -@ManyToMany(@OneToMany)

--- src/main/java/hellojpa/Product.java
+++ src/main/java/hellojpa/Product.java
@@ -17,8 +17,8 @@ public class Product {
     // 다대다 매핑
     // product 테이블의 products와 매핑
     // 1:1과 비슷하지만 중간 테이블 하나 생긴다는 차이점
-    @ManyToMany(mappedBy = "products")
-    private List<Member> members = new ArrayList<>();
+//    @ManyToMany(mappedBy = "products")
+//    private List<Member> members = new ArrayList<>();
 
     // 다대다를 일대다, 다대다로 풀기
     /*@OneToMany(mappedBy = "product")
