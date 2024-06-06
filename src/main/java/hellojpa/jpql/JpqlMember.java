0d5554caa77b31f74dc64ed37da6f6a7d114959d
idohyeon <ehgus5026@naver.com>
2024-06-07 03:06:33 +0900

    -JPQL

---
+++ src/main/java/hellojpa/jpql/JpqlMember.java
@@ -0,0 +1,78 @@
+package hellojpa.jpql;
+
+import jakarta.persistence.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+public class JpqlMember {
+
+    @Id @GeneratedValue
+    private Long id;
+
+    private String username;
+    private int age;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "TEAM_ID")
+    private Team team;
+
+    @Enumerated(EnumType.STRING)
+    private MemberType type;
+
+    public void addTeam(Team team) {
+        this.team = team;
+        team.getMembers().add(this);
+    }
+
+    public MemberType getType() {
+        return type;
+    }
+
+    public void setType(MemberType type) {
+        this.type = type;
+    }
+
+    public Team getTeam() {
+        return team;
+    }
+
+    public void setTeam(Team team) {
+        this.team = team;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    @Override
+    public String toString() {
+        return "JpqlMember{" +
+                "id=" + id +
+                ", username='" + username + '\'' +
+                ", age=" + age +
+                '}';
+    }
+
+}
